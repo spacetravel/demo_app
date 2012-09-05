bb6c3d7706c438ef6445e2373cdef83f1aadb734
Spacetravel Øyvind <spacetravel.no@gmail.com>
2012-09-05 23:29:34 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9d169a953ee3a5f51bc69e6a8c87990f8e20bcd148f9d70d6bf4e7d1890928debbd79b751bcf81fefd52d09a3da534a06e0b89e06a7cd41cf45367e5c12b8e0d'
